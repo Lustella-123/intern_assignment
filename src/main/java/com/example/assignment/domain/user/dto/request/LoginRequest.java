db95cf03296c5b82f4073a320565dd5096ecb155
tella <yung12366@gmail.com>
2025-04-19 13:02:09 +0900

    [refactor] 테스트를 위한 애노테이션 추가

--- src/main/java/com/example/assignment/domain/user/dto/request/LoginRequest.java
+++ src/main/java/com/example/assignment/domain/user/dto/request/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.example.assignment.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
